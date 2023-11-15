777ef49a6b2ce827d7dd8b1aca94b85af499607e
Vu Du <vudu200746@gmail.com>
2023-11-15 11:53:37 -0600

    More progress on player selection screen

--- Assets/Scripts/UI/PlayerSelectMenu.cs
+++ Assets/Scripts/UI/PlayerSelectMenu.cs
@@ -1,13 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+static class PlayerSelectMenuConstants {
+	public const int MAX_PLAYER_COUNT = 4;
+}
 
 public class PlayerSelectMenu : MonoBehaviour {
 
 	[SerializeField] private GameObject playerInfoBoxPrefab;
 	[SerializeField] private GameObject location;
+	[SerializeField] private string mainMenuSceneName;
+	[SerializeField] private string startGameSceneName;
+
+	private static int playerCount;
 
 	private void Start() {
-		// Instantiate(playerInfoBoxPrefab, Vector3.one, Quaternion.identity, location.transform);
+		// Start with 1 player
+		playerCount = 1;
+		Instantiate(playerInfoBoxPrefab, Vector3.one, Quaternion.identity, location.transform);
+	}
+
+	public void GoToMainMenu() {
+		SceneManager.LoadScene(mainMenuSceneName);
+	}
+
+	public void StartGame() {
+		SceneManager.LoadScene(startGameSceneName);
 	}
 }
